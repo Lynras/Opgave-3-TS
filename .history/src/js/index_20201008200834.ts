ffc3a3dc189a7ca72b8c63a8d5cc9751491f5c54
Rasmus Deckmann <rasm732d@edu.easj.dk>
2020-10-09 07:42:55 +0200

    first commit

---
+++ .history/src/js/index_20201008200834.ts
@@ -0,0 +1,57 @@
+let Selector: HTMLSelectElement = <HTMLSelectElement>(
+  document.getElementById("Selector")
+);
+let element: HTMLDivElement = <HTMLDivElement>(
+  document.getElementById("content")
+);
+let Manipulate: HTMLButtonElement = <HTMLButtonElement>(
+  document.getElementById("Manipulator")
+);
+let Input: HTMLInputElement = <HTMLInputElement>(
+  document.getElementById("TekstInput")
+);
+
+Manipulate.addEventListener("click", function () {
+  element.innerHTML = manipulateString(Input.value);
+});
+
+element.innerHTML = Input.value;
+
+function manipulateString(input: string): string {
+  switch (Selector.selectedIndex) {
+    case 0: {
+      return input.toLowerCase();
+    }
+
+    case 1: {
+      return input.toUpperCase();
+    }
+
+    default:
+      return input;
+  }
+}
+
+// import { Vue } from "../../node_modules/vue/types/vue";
+// new Vue({
+//   el: "#app",
+//   data: {
+//     input: "",
+//     selected: "",
+//     result: "",
+//   },
+//   methods: {
+//     ChangeCase() {
+//       switch (this.selected) {
+//         case "Lower Case":
+//           this.result = this.input.toLowerCase();
+//           break;
+
+//         case "Upper Case":
+//           this.result = this.input.toUpperCase();
+//           break;
+//       }
+//       this.result = "";
+//     },
+//   },
+// });
---
+++ .history/src/js/index_20201008221402.ts
@@ -0,0 +1,59 @@
+// let Selector: HTMLSelectElement = <HTMLSelectElement>(
+//   document.getElementById("Selector")
+// );
+// let element: HTMLDivElement = <HTMLDivElement>(
+//   document.getElementById("content")
+// );
+// let Manipulate: HTMLButtonElement = <HTMLButtonElement>(
+//   document.getElementById("Manipulator")
+// );
+// let Input: HTMLInputElement = <HTMLInputElement>(
+//   document.getElementById("TekstInput")
+// );
+
+import { Vue } from "../../node_modules/vue/types/vue";
+
+// Manipulate.addEventListener("click", function () {
+//   element.innerHTML = manipulateString(Input.value);
+// });
+
+// element.innerHTML = Input.value;
+
+// function manipulateString(input: string): string {
+//   switch (Selector.selectedIndex) {
+//     case 0: {
+//       return input.toLowerCase();
+//     }
+
+//     case 1: {
+//       return input.toUpperCase();
+//     }
+
+//     default:
+//       return input;
+//   }
+// }
+
+new Vue({
+  el: "#app",
+  data: {
+    input: "",
+    selected: "",
+    content: "",
+  },
+
+  methods: {
+    manipulateString() {
+      switch (this.selected) {
+        case "Upper Case":
+          this.content = this.Input.toUpperCase();
+          break;
+
+        case "Lower Case":
+          this.content = this.Input.toLowerCase();
+          break;
+      }
+      this.content = "";
+    },
+  },
+});
